9c01c6f95653af70adc1e6dbad3c094f85b30601
BR160ASP <sashila@ad.bradley.edu>
2019-01-25 17:50:51 -0600

    Progress_cmplt

--- Pusher_chat/pusherApp/src/app/chat/chat.component.ts
+++ Pusher_chat/pusherApp/src/app/chat/chat.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Ichat } from '../interfaces/ichat';
+import { ChatService } from '../services/chat.service';
 
 @Component({
   selector: 'app-chat',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
+  chats: Ichat[] = [];
+  message: string;
+  sending: boolean;
 
-  constructor() { }
+  constructor(private _chatService: ChatService) { }
 
   ngOnInit() {
+    this._chatService.getChannel().bind('chat', data => {
+      if (data.email === this._chatService.user.email) {
+        data.isMe = true;
+      }
+      this.chats.push(data);
+    });
+  }
+
+  sendMessage(message: string) {
+    this.sending = true;
+    this._chatService.sendMessage(message)
+      .subscribe(resp => {
+        this.message = undefined;
+        this.sending = false;
+      }, err => {
+        this.sending = false;
+      } );
   }
 
 }
